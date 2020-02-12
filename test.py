3cafd889f4074ebcc75cdcdd4d27965a087b14f2
thaduri-karunakar <reddy.karnakar4@gmail.com>
2020-02-12 19:34:19 +0800

    added hostname command

--- test.py
+++ test.py
@@ -1,8 +1,13 @@
 import os
+import subprocess
 robotList = ['myChinarobot','myBhutanrobot','myBurmarobot','myIndiarobot','myJapanrobot','myNKorearobot',
              'mySKorearobot','mySrilankarobot', 'myVietnamrobot', 'myIranrobot', 'myIraqrobot', 'myIsraelrobot',
              'myJordanrobot','myTaiwanrobot', 'myOmanrobot', 'myNepalrobot', 'myQatarrobot', 'myYemenrobot','myThailandrobot']
 for i in robotList:
     print (i)
-print (os.system('hostname'))
+
+proc = subprocess.Popen(["hostname"], stdout=subprocess.PIPE, shell=True)
+(out, err) = proc.communicate()
+print('*'*40)
+print ("program output:", out)
 print('above line should be hostname')
